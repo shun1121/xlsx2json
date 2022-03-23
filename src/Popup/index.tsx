4b8407590438c94ea42a06932f45786a3d8bf23d
shun1121 <hikitanikun.ware80000@gmail.com>
2022-03-24 01:43:31 +0900

    enabled to choose sheet number and cell

--- src/Popup/index.tsx
+++ src/Popup/index.tsx
@@ -2,62 +2,61 @@ import React, { useState } from "react";
 const XLSX = require("xlsx");
 
 export const Popup: React.VFC = () => {
-  const [workbook, setWorkbook] = useState() as any
-  const [sheetNumber, setSheetNumber] = useState() as any
-  const [cellNumber, setCellNumber] = useState() as any
+  const [workbook, setWorkbook] = useState() as any;
+  const [sheetNumber, setSheetNumber] = useState() as any;
+  const [cellNumber, setCellNumber] = useState() as any;
+  const [data, setData] = useState() as any;
 
   const handleReadFile = async (fileObject: File | null) => {
     if (fileObject) {
-      const buffer = await fileObject.arrayBuffer()
-      await setWorkbook(XLSX.read(buffer))
+      const buffer = await fileObject.arrayBuffer();
+      await setWorkbook(XLSX.read(buffer));
     }
-  }
+  };
 
   const handleChange = (e: any) => {
-    e.preventDefault()
-    console.log(e)
-    console.log(e.currentTarget)
-    console.log(e.currentTarget.files[0])
-    handleReadFile(e.currentTarget.files[0])
-  }
+    e.preventDefault();
+    console.log(e.currentTarget.files[0]);
+    handleReadFile(e.currentTarget.files[0]);
+  };
 
   const chooseSheet = (e: any) => {
-    const Sheet = 'Sheet' + e.target.value
-    console.log(Sheet)
-    setSheetNumber(Sheet)
-  }
-  // const sheetNum = document.getElementById("sheetNum")
-  // const sheetFunc:any = sheetNum?.addEventListener("change", chooseSheet)
-  // console.log(sheetFunc)
+    const Sheet = "Sheet" + e.target.value;
+    console.log(Sheet);
+    setSheetNumber(Sheet);
+  };
 
   const chooseCell = (e: any) => {
-    const cell = e.target.value
-    const selected_cell = cell[0].toUpperCase() + cell.slice(1)
-    const sheet_list = workbook.SheetNames
-    console.log(sheet_list)
-    setCellNumber(selected_cell)
-    const Sheet1 = workbook.Sheets[sheet_list[0]]
-    // console.log(Sheet1)
-    // console.log(sheetFunc)
-    const targetCell = Sheet1[selected_cell].v
-    // const targetCell = sheetNumber[selected_cell].v
-    console.log(`シート1のセル${selected_cell}の値：\n${targetCell}`)
-  }
-  const cellNum = document.getElementById("cellNum")
-  cellNum?.addEventListener("change", chooseCell)
+    const cell = e.target.value;
+    const selected_cell = cell[0].toUpperCase() + cell.slice(1);
+    setCellNumber(selected_cell);
+  };
+  const cellNum = document.getElementById("cellNum");
+  cellNum?.addEventListener("change", chooseCell);
+
+  const handleSubmit = (e: any) => {
+    const Sheet = workbook.Sheets[sheetNumber];
+    const targetCell = Sheet[cellNumber].v;
+    console.log(`シート1のセル${cellNumber}の値：\n${targetCell}`);
+    setData(targetCell);
+    e.preventDefault();
+  };
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <input type="file" onChange={handleChange} />
-      <div>
-        <span>sheet number: </span><input type="text" id="sheetNum" onChange={chooseSheet} />
-      </div>
-      <div>
-        <span>choose cell: </span><input type="text" id="cellNum" />
-      </div>
-      {/* <div>
-        <span>end: </span><input type="text"/>
-      </div> */}
+      <form onSubmit={handleSubmit}>
+        <label>
+          sheet number:
+          <input type="text" onChange={chooseSheet} />
+        </label>
+        <label>
+          choose cell:
+          <input type="text" onChange={chooseCell} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {cellNumber ? <div>{data}</div> : <></>}
     </div>
-  )
+  );
 };
