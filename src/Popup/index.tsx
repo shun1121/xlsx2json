94a09c99512d96cbd2dd46a9d5ee713403c73768
shun1121 <hikitanikun.ware80000@gmail.com>
2022-03-24 20:45:03 +0900

    used selectbox for choosing sheet number

--- src/Popup/index.tsx
+++ src/Popup/index.tsx
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 const XLSX = require("xlsx");
 
+type Type = {
+  [key: string]: {
+    [key: string]: {
+      [key: string]: {
+        h: string;
+        r: string;
+        t: string;
+        v: string;
+        w: string;
+      };
+    };
+  };
+}
+
 export const Popup: React.VFC = () => {
-  const [workbook, setWorkbook] = useState() as any;
-  const [sheetNumber, setSheetNumber] = useState() as any;
-  const [cellNumber, setCellNumber] = useState() as any;
-  const [data, setData] = useState() as any;
+  const [workbook, setWorkbook] = useState<Type>({});
+  const [sheetNumber, setSheetNumber] = useState("");
+  const [cellNumber, setCellNumber] = useState("");
+  const [value, setValue] = useState("")
+  const [sheetList, setSheetList] = useState([])
+  const [data, setData] = useState("");
+
+  const sheet_number = [
+    ['1', 1],
+    ['2', 2],
+    ['3', 3],
+    ['4', 4],
+    ['5', 5],
+  ]
 
   const handleReadFile = async (fileObject: File | null) => {
     if (fileObject) {
@@ -14,49 +38,81 @@ export const Popup: React.VFC = () => {
     }
   };
 
-  const handleChange = (e: any) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
-    console.log(e.currentTarget.files[0]);
-    handleReadFile(e.currentTarget.files[0]);
+    const fileObject: File | null = e.currentTarget.files![0]
+    handleReadFile(fileObject);
   };
 
-  const chooseSheet = (e: any) => {
-    const Sheet = "Sheet" + e.target.value;
-    console.log(Sheet);
-    setSheetNumber(Sheet);
-  };
+  // const chooseSheet = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const Sheet = "Sheet" + e.target.value;
+  //   console.log(typeof e.target.value)
+  //   console.log(Sheet)
+  //   setSheetNumber(Sheet);
+  // };
 
-  const chooseCell = (e: any) => {
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const sheet_list: any = workbook.SheetNames
+    setSheetList(sheet_list.length)
+    console.log("↓")
+    console.log(sheet_list.length)
+    console.log(workbook)
+    console.log(e.target.value)
+    setSheetNumber("Sheet" + e.target.value)
+  }
+  console.log(sheetNumber)
+
+  const chooseCell = (e: React.ChangeEvent<HTMLInputElement>) => {
     const cell = e.target.value;
     const selected_cell = cell[0].toUpperCase() + cell.slice(1);
-    setCellNumber(selected_cell);
+    console.log(typeof cell.slice(1))
+    console.log(selected_cell)
+    if (selected_cell) {
+      setCellNumber(selected_cell);
+    } else {
+      console.log('input value is undefined')
+    }
   };
-  const cellNum = document.getElementById("cellNum");
-  cellNum?.addEventListener("change", chooseCell);
 
-  const handleSubmit = (e: any) => {
-    const Sheet = workbook.Sheets[sheetNumber];
-    const targetCell = Sheet[cellNumber].v;
-    console.log(`シート1のセル${cellNumber}の値：\n${targetCell}`);
-    setData(targetCell);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    console.log(workbook);
+    console.log(workbook.Sheets);
+    const Sheet = workbook.Sheets[sheetNumber];
+    console.log(Sheet)
+    const targetCell = Sheet[cellNumber].v;
+    setData(targetCell);
   };
 
   return (
     <div style={{ padding: "20px" }}>
       <input type="file" onChange={handleChange} />
       <form onSubmit={handleSubmit}>
-        <label>
+        {/* <label>
           sheet number:
           <input type="text" onChange={chooseSheet} />
-        </label>
-        <label>
-          choose cell:
-          <input type="text" onChange={chooseCell} />
-        </label>
-        <input type="submit" value="Submit" />
+        </label> */}
+        <select value={sheetNumber} onChange={handleSelect}>
+          <option value="">選択してください</option>
+          {sheet_number.map(option => (
+            <option key={option[0]} value={option[0]}>{option[1]}</option>
+          ))}
+          {/* <option value="grapefruit">Grapefruit</option>
+          <option value="lime">Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option> */}
+        </select>
+        <div>
+          <label>
+            choose cell:
+            <input type="text" onChange={chooseCell} />
+          </label>
+        </div>
+        <div>
+          <input type="submit" value="Submit" />
+        </div>
       </form>
-      {cellNumber ? <div>{data}</div> : <></>}
+      {data ? <div>{`シート1のセル${cellNumber}の値：\n${data}`}</div> : <></>}
     </div>
   );
 };
